da3b3422be4d3b417ff09fe81db77a58a0a722e9
Zoë Blade <zoe@bytenoise.co.uk>
2012-05-09 22:33:00 +0100

    Added definitions.

--- cpp/Sequencer.h
+++ cpp/Sequencer.h
@@ -1,6 +1,18 @@
 #ifndef SEQUENCER_H
 #define SEQUENCER_H
 
+#ifndef MAX_NUMBER_OF_PATTERNS
+#define MAX_NUMBER_OF_PATTERNS 64
+#endif
+
+#ifndef MAX_NUMBER_OF_ROWS
+#define MAX_NUMBER_OF_ROWS 64
+#endif
+
+#ifndef NUMBER_OF_CHANNELS
+#define NUMBER_OF_CHANNELS 4
+#endif
+
 class Sequencer {
 };
 
--- cpp/stepper_3_arduino.cpp
+++ cpp/stepper_3_arduino.cpp
@@ -1,3 +1,7 @@
+#define MAX_NUMBER_OF_PATTERNS 16
+#define MAX_NUMBER_OF_ROWS 16
+#define NUMBER_OF_CHANNELS 1
+
 #include "Sequencer.cpp"
 
 void setup() {
