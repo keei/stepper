7d9c2451224aed00a45ba49870955566e215dcf1
Zoë Blade <zoe@bytenoise.co.uk>
2012-05-11 12:41:52 +0100

    Seeing how I can cope without curses.
    Not too well, as it happens.

--- cpp/stepper_1_prototype.cpp
+++ cpp/stepper_1_prototype.cpp
@@ -3,22 +3,25 @@
 #define MAX_NUMBER_OF_ROWS 64
 #define NUMBER_OF_CHANNELS 1
 
-#include <curses.h>
 #include <stdio.h>
 #include "Sequencer.cpp"
 
 int main() {
 	unsigned short previousCycleTimeInSeconds = 0;
+	char key = ' ';
 
 	Sequencer sequencer;
 	sequencer.slideCV1 = false; // This won't have a DAC
 	sequencer.slideCV2 = false; // This won't exist at all
 
-	initscr();
-
-	while (true) {
-		if (true) {
-			endwin();
+	while (key = getchar()) {
+		if (key == 'q') {
+			return 0;
 		}
+
+		printf("Pitch: %u\n", sequencer.getPitchInTwelveBits(0));
+		printf("Gate:  %u\n", sequencer.getGateInTwelveBits(0));
+		printf("CV1:   %u\n", sequencer.getCV1InTwelveBits(0));
+		printf("CV1:   %u\n", sequencer.getCV2InTwelveBits(0));
 	}
 }
