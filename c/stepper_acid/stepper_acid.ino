dfc11b9111f3336d378f833e2fab89e13f411193
Zoë Blade <zoe@bytenoise.co.uk>
2012-05-27 10:55:37 +0100

    Started to implement C port of sequencer.
    So far the Arduino just acts as a metronome, but behind the scenes,
    it's a 96 PPQN clock driving a 48 PPQN step sequencer, which makes
    the LED light up on rows 0, 4, 8 and 12.
    
    Squashed commit of the following:
    
    commit 57818a0003dcc5360767ec9f8303663e556b5372
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 10:50:05 2012 +0100
    
        Made the LED flash.
    
        The Arduino's internal LED now flashes on every beat, just so I can
        verify that something's actually happening.  :)
    
    commit d0a0454890e9422a71187421887dc63cbeb4e8ef
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 10:45:56 2012 +0100
    
        More sequencer implementation.
    
        I'm using functions for loadPattern() and savePattern() because
        they're called absolutely everywhere.
    
    commit d26a46653ce7e775dee847eda0f3df2ae2f08dd3
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 10:40:20 2012 +0100
    
        Started work on the sequencer.
    
        Again, everything's in the main loop now, not farmed out to functions,
        let alone objects.
    
    commit b9588617d85ae6a45a64b3a1ab6794e36680c2ab
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 10:20:49 2012 +0100
    
        Variable sharing.
    
    commit d324282423faf40a412738d5340d3dfd95bb83c6
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 10:15:39 2012 +0100
    
        More deOOPifying.
    
        Encouraging the different parts of the system to share each other's
        variables, in order to shave off valuable CPU cycles.  Getting pretty
        low level now.
    
        I'll probably finish Stepper Acid before I properly start work on
        Stepper Drum, so I don't have to keep on copying and pasting updates.

--- c/stepper_acid/stepper_acid.ino
+++ c/stepper_acid/stepper_acid.ino
@@ -4,8 +4,19 @@
  *  Clock constants
  */
 
-#define CLOCK_DEFAULT_TEMPO 120 /* In BPM */
 #define CLOCK_FROM_TEMPO_TO_MILLISECONDS 625 /* 60 seconds per minute; 60 / BPM = quarter note length in seconds; 60,000 / BPM = quarter note length in milliseconds; quarter note length in milliseconds / 96 = clock pulse length in milliseconds; 60,000 / 96 = 625; 625 / BPM = pulse length in milliseconds */
+
+/*
+ *  Sequencer constants
+ */
+
+#define SEQUENCER_PPSN 12 /* 48 PPQN = 12 PPSN */
+
+/*
+ *  Main variables
+ */
+
+unsigned char oddPulse = 1; /* Think of it as starting at -1 */
 
 /*
  *  Clock variables
@@ -13,11 +24,24 @@
 
 unsigned long clockLastTime = 0; /* When the last pulse started, in milliseconds */
 unsigned char clockPulse = HIGH; /* The clock pulse, 96 PPQN.  Count 1 in 2 for 48 PPQN, or 1 in 4 for 24 PPQN. */
+unsigned char clockPulseStarting = HIGH; /* Whether the clock pulse is going from low to high on this exact iteration of the main loop */
 unsigned long clockPulseLength = 5; /* The amount of time between one pulse start and the next, in milliseconds */
 unsigned char clockRun = LOW; /* Whether or not we're running */
 unsigned char clockRunRequest = LOW; /* Whether or not we're about to run */
-unsigned char clockTempo = CLOCK_DEFAULT_TEMPO; /* In BPM */
+unsigned char clockTempo = 120; /* In BPM */
 unsigned long clockTime = 0; /* The current time, in milliseconds */
+
+/*
+ *  Sequencer variables
+ */
+
+unsigned char currentPatternNumber = 0;
+unsigned char currentRowNumber = 0;
+unsigned char lastRowNumber = 0;
+unsigned char newCurrentRowNumber = 0;
+unsigned char nextPatternNumber = 0;
+unsigned char numberOfRows = 16;
+unsigned int sequencerPulseCount = 0; /* The number of pulses that have happened so far in this pattern.  Note that although the internal clock is 96 PPQN, the sequencer is only 48 PPQN.  This is so that both can be compatible with other machines. */
 
 /*
  *  Slew limiter variables
@@ -26,49 +50,52 @@ unsigned long clockTime = 0; /* The current time, in milliseconds */
 unsigned long slewLimiterLastTime = 0; /* When the slew limiter was last invoked, in milliseconds */
 unsigned long slewLimiterInterval = 0; /* The distance between last time and this time, in milliseconds */
 unsigned char slewLimiterPitch = 0; /* The actual pitch CV output of the slew limiter, in 12 bits */
-unsigned char slewLimiterPitchRequest = 0; /* The proposed pitch CV output of the slew limiter, in 12 bits */
-unsigned char slewLimiterLag = 0; /* Whether the slew limiter should lag or not */
-unsigned long slewLimiterThisTime = 0; /* Now, in milliseconds */
+unsigned char pitchRequest = 0; /* The proposed pitch CV output of the slew limiter, in 12 bits */
+unsigned char slide = 0; /* Whether the slew limiter should lag or not */
 
 /*
- *  Clock functions
+ *  Custom functions
  */
 
-void clockDecrementTempo()
+void loadPattern()
 {
-	if (clockTempo > 1) {
-		clockTempo--;
-		clockPulseLength = CLOCK_FROM_TEMPO_TO_MILLISECONDS / clockTempo;
-	}
 }
 
-void clockIncrementTempo()
+void savePattern()
 {
-	if (clockTempo < 255) {
-		clockTempo++;
-		clockPulseLength = CLOCK_FROM_TEMPO_TO_MILLISECONDS / clockTempo;
-	}
 }
 
-void clockSetTempo(unsigned char t)
+/*
+ *  Initialise at bootup
+ */
+
+void setup()
 {
-	if (0 < t < 256) {
-		clockTempo = t;
-		clockPulseLength = CLOCK_FROM_TEMPO_TO_MILLISECONDS / clockTempo;
-	}
+	pinMode(13, OUTPUT);
 }
 
-void clockUpdate()
+/*
+ *  Main loop
+ */
+
+void loop()
 {
+
+/*
+ *  Update the clock
+ */
+
 	clockTime = millis();
 
 	if (clockTime - clockLastTime < clockPulseLength / 2) {
 		/* The last pulse is still happening */
 		clockPulse = HIGH;
+		clockPulseStarting = LOW;
 	} else if (clockTime - clockLastTime > clockPulseLength) {
 		/* The next pulse is already happening */
 		clockPulse = HIGH;
 		clockLastTime = clockTime;
+		clockPulseStarting = HIGH;
 
 		/* Run/stop requests can only be fulfilled at the start of a new clock pulse */
 		if (clockRunRequest == HIGH) {
@@ -79,45 +106,81 @@ void clockUpdate()
 	} else {
 		/* Neither pulse is happening */
 		clockPulse = LOW;
+		clockPulseStarting = LOW;
 	}
-}
 
 /*
- *  Slew limiter functions
+ *  Update the sequencer
  */
 
-/* Make sure slewLimiterPitchRequest and slewLimiterLag are set appropriately first */
-void slewLimiterSetPitch()
-{
-	slewLimiterThisTime = millis();
-	slewLimiterInterval = slewLimiterThisTime - slewLimiterLastTime;
-	slewLimiterLastTime = slewLimiterThisTime;
+	if (clockPulseStarting == HIGH) {
+		oddPulse = oddPulse ^ 1; /* I'm guessing there's no such thing as "oddPulse =^ 1"? */
 
-	if (slewLimiterLag == LOW) {
-		slewLimiterPitch = slewLimiterPitchRequest;
-	} else {
-		if (slewLimiterPitchRequest > slewLimiterPitch + (slewLimiterInterval * 2)) {
-			slewLimiterPitch = slewLimiterPitch + slewLimiterInterval;
-		} else if (slewLimiterPitchRequest < slewLimiterPitch - (slewLimiterInterval * 2)) {
-			slewLimiterPitch = slewLimiterPitch - slewLimiterInterval;
-		} else {
-			slewLimiterPitch = slewLimiterPitchRequest;
+		if (oddPulse == 0) {
+			sequencerPulseCount++;
+
+			if (sequencerPulseCount > SEQUENCER_PPSN * numberOfRows) {
+				sequencerPulseCount = 0;
+				currentPatternNumber = nextPatternNumber;
+				loadPattern();
+			}
+
+			newCurrentRowNumber = sequencerPulseCount / SEQUENCER_PPSN;
+
+			if (newCurrentRowNumber != currentRowNumber) {
+				lastRowNumber = currentRowNumber;
+			}
+
+			currentRowNumber = newCurrentRowNumber;
 		}
 	}
-}
 
 /*
- *  Example main code
+ *  Run the pitch through the slew limiter
  */
 
-void setup()
-{
-	pinMode(13, OUTPUT);
-	clockSetTempo(120);
-}
+/* Make sure pitchRequest and slide are set appropriately by the sequencer first */
+	slewLimiterInterval = clockTime - slewLimiterLastTime;
+	slewLimiterLastTime = clockTime;
 
-void loop()
-{
-	clockUpdate();
-	digitalWrite(13, clockPulse);
+	if (slide == 0) {
+		slewLimiterPitch = pitchRequest;
+	} else {
+		if (pitchRequest > slewLimiterPitch + (slewLimiterInterval * 2)) {
+			slewLimiterPitch = slewLimiterPitch + slewLimiterInterval;
+		} else if (pitchRequest < slewLimiterPitch - (slewLimiterInterval * 2)) {
+			slewLimiterPitch = slewLimiterPitch - slewLimiterInterval;
+		} else {
+			slewLimiterPitch = pitchRequest;
+		}
+	}
+
+/*
+ *  Set output
+ */
+
+	/* Flash the Arduino's internal LED on every beat */
+	if (currentRowNumber % 4 == 0) {
+		digitalWrite(13, HIGH);
+	} else {
+		digitalWrite(13, LOW);
+	}
+
+/*
+ *  Get input
+ */
+
+	if (0) { /* Decrement the clock's tempo */
+		if (clockTempo > 1) {
+			clockTempo--;
+			clockPulseLength = CLOCK_FROM_TEMPO_TO_MILLISECONDS / clockTempo;
+		}
+	}
+
+	if (0) { /* Increment the clock's tempo */
+		if (clockTempo < 255) {
+			clockTempo++;
+			clockPulseLength = CLOCK_FROM_TEMPO_TO_MILLISECONDS / clockTempo;
+		}
+	}
 }
