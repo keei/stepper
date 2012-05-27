33b0b948c3291c7f3d5a464d0f40fd23c81093e5
Zoë Blade <zoe@bytenoise.co.uk>
2012-05-27 14:35:48 +0100

    Implemented sequencer in Arduino C.
    It doesn't have an interfcae yet, but it does have a preset pattern.
    
    Squashed commit of the following:
    
    commit bfcd89251a866da548aec006e72a1cfc7acce833
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 14:35:13 2012 +0100
    
        Commented out DAC writing line.
    
        I don't yet know if this works.
    
    commit 94e7310bc73c3009c955833ff288dead197add3a
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 14:32:53 2012 +0100
    
        Added DAC function.
    
        Theoretically, it should now be able to write to a DAC, were one
        installed.
    
        Let's find out later on...
    
        So far, it just stops the LED flashing, although the code does compile
        without errors.  But then again, we haven't installed a DAC yet.
    
    commit abe6b42f80296d9060489b1ff7e798afe0c83217
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 14:22:32 2012 +0100
    
        No it doesn't.
    
        Looping a pattern should *not* cause it to reload, that would be too
        much overhead.  I put in a condition for that a few commits ago.
    
        Revert "See comment."
    
        This reverts commit 0a82db1a3c84da5fb1805784d6b3c66941d2ad6e.
    
    commit 0a82db1a3c84da5fb1805784d6b3c66941d2ad6e
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 14:22:27 2012 +0100
    
        See comment.
    
    commit 4a883bdf576ff014e6c0149b511105991f43a2a9
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 14:17:37 2012 +0100
    
        Renaming variables.
    
        Again, this is to bring them in line with other varible names.
    
    commit 354561f86740af7a13cbd3ab6c9b4070fc73cdc0
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 14:15:08 2012 +0100
    
        Commenting, refactoring.
    
    commit d10dd3ba059fcafa7c4346c655cd5221625dcf35
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 14:12:01 2012 +0100
    
        Bugfixes.
    
        Bearing in mind my only means of testing this is by changing what
        gets echoed out to the built-in LED.  :)
    
    commit bc422f4321ce9122fd11cc9e6d6f06ef297a6c10
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 14:06:52 2012 +0100
    
        (Hopefully) programmed in a preset pattern.
    
        The final version probably shouldn't do this, but it's necessary
        before we've added an interface to let people program in their own
        patterns.
    
    commit 232d3ba90ea710bde5af75af15e2d67b0d51c36c
    Merge: b671b71 30bc8fe
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 12:33:12 2012 +0100
    
        Merge branch 'master' into c-seq
    
    commit b671b71908bb17ac076a1435040525807b093b15
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 12:32:06 2012 +0100
    
        Implemented the run/stop button.
    
    commit 8a19e68bc68bd53b94b42864da2900da9605045f
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 12:25:59 2012 +0100
    
        (Theoretically) implemented the clipboard etc.
    
    commit 1f779b696e1ab55a1ee049ad3b2fb9934da00039
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 12:06:40 2012 +0100
    
        Minor renaming and refactoring.
    
    commit beaf51414ba1c7454874b4022f3ab762e2b91cd2
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 11:57:17 2012 +0100
    
        (Theoretically) added digital output of gate and accent.
    
    commit d08cfa1114e9185576d2de526474845249d66acf
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 11:46:51 2012 +0100
    
        Accent is now read from the pattern buffer.
    
    commit 03bdcf043ddf3984b34379fe9151b01f204d7b93
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 11:45:11 2012 +0100
    
        Translated more of sequencer.
    
    commit 4778a629187ccfae2a21d8c6eb8b9652d1845ac2
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 11:24:29 2012 +0100
    
        Refactored.
    
        Less variable copying.  Less simple, but faster.
    
    commit e95d67e0c261abb18d5f4143b9e158929395db67
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Sun May 27 11:22:28 2012 +0100
    
        More translating, about to refactor.

--- c/stepper_acid/stepper_acid.ino
+++ c/stepper_acid/stepper_acid.ino
@@ -1,22 +1,48 @@
+/*
+ *  Stepper Acid: an acid machine with CV and gate outputs.
+ *  By Zoe Blade and Nina Richards.
+ *
+ *  Digital pin 2 is gate.
+ *  Digital pin 3 is accent.
+ */
+
 #include "Arduino.h"
+#include "Wire.h"
 
 /*
  *  Clock constants
  */
 
-#define CLOCK_FROM_TEMPO_TO_MILLISECONDS 625 /* 60 seconds per minute; 60 / BPM = quarter note length in seconds; 60,000 / BPM = quarter note length in milliseconds; quarter note length in milliseconds / 96 = clock pulse length in milliseconds; 60,000 / 96 = 625; 625 / BPM = pulse length in milliseconds */
+#define FROM_TEMPO_TO_MILLISECONDS 625 /* 60 seconds per minute; 60 / BPM = quarter note length in seconds; 60,000 / BPM = quarter note length in milliseconds; quarter note length in milliseconds / 96 = clock pulse length in milliseconds; 60,000 / 96 = 625; 625 / BPM = pulse length in milliseconds */
 
 /*
  *  Sequencer constants
  */
 
+#define FROM_SIXTIETHS_TO_PULSES 0.2 /* Multiplying by 0.8 is the same as dividing by 60 and multiplying by 12, as in 12 PPSNs */
+#define FROM_SIXTIETHS_TO_TWELVE_BITS 68.25 /* Multiplying by 68.25 is the same as dividing by 60 and multiplying by 4095 */
+#define MAX_NUMBER_OF_ROWS 16
 #define SEQUENCER_PPSN 12 /* 48 PPQN = 12 PPSN */
+
+/* Array indeces */
+#define PITCH 0
+#define SLIDE 1
+#define GATE 2
+#define ACCENT 3
+
+/*
+ *  Universal, disposable variables
+ *  These can be used and overwritten at any time by anything.  Be careful!
+ */
+
+unsigned char row = 0;
+unsigned char item = 0;
 
 /*
  *  Main variables
  */
 
-unsigned char oddPulse = 1; /* Think of it as starting at -1 */
+unsigned char oddPulse = 1; /* Think of it as starting at -1, getting ready to increment to 0 in the first iteration */
 
 /*
  *  Clock variables
@@ -35,13 +61,21 @@ unsigned long clockTime = 0; /* The current time, in milliseconds */
  *  Sequencer variables
  */
 
-unsigned char currentPatternNumber = 0;
-unsigned char currentRowNumber = 0;
+unsigned char accent = LOW;
+unsigned char clipboard[MAX_NUMBER_OF_ROWS][4]; /* 4: Pitch, slide, gate, accent */
+unsigned char clipboardFull = LOW;
+unsigned char clipboardNumberOfRows = MAX_NUMBER_OF_ROWS;
+unsigned char gate = LOW;
 unsigned char lastRowNumber = 0;
 unsigned char newCurrentRowNumber = 0;
-unsigned char nextPatternNumber = 0;
-unsigned char numberOfRows = 16;
-unsigned int sequencerPulseCount = 0; /* The number of pulses that have happened so far in this pattern.  Note that although the internal clock is 96 PPQN, the sequencer is only 48 PPQN.  This is so that both can be compatible with other machines. */
+unsigned char patternNumber = 0;
+unsigned char patternNumberRequest = 0;
+unsigned char numberOfRows = MAX_NUMBER_OF_ROWS;
+unsigned char pattern[MAX_NUMBER_OF_ROWS][4]; /* 4: Pitch, slide, gate, accent */
+unsigned char pitchRequest = 0; /* Used by both the sequencer and slew limiter.  The proposed pitch CV output of the slew limiter, in 12 bits. */
+unsigned char rowNumber = 0;
+unsigned short sequencerPulseCount = 0; /* The number of pulses that have happened so far in this pattern.  Note that although the internal clock is 96 PPQN, the sequencer is only 48 PPQN.  This is so that both can be compatible with other machines. */
+unsigned char slide = LOW; /* Used by both the sequencer and slew limiter.  Whether the slew limiter should lag or not. */
 
 /*
  *  Slew limiter variables
@@ -49,9 +83,7 @@ unsigned int sequencerPulseCount = 0; /* The number of pulses that have happened
 
 unsigned long slewLimiterLastTime = 0; /* When the slew limiter was last invoked, in milliseconds */
 unsigned long slewLimiterInterval = 0; /* The distance between last time and this time, in milliseconds */
-unsigned char slewLimiterPitch = 0; /* The actual pitch CV output of the slew limiter, in 12 bits */
-unsigned char pitchRequest = 0; /* The proposed pitch CV output of the slew limiter, in 12 bits */
-unsigned char slide = 0; /* Whether the slew limiter should lag or not */
+unsigned char pitch = 0; /* The actual pitch CV output of the slew limiter, in 12 bits */
 
 /*
  *  Custom functions
@@ -59,10 +91,30 @@ unsigned char slide = 0; /* Whether the slew limiter should lag or not */
 
 void loadPattern()
 {
+/* This ought to load the pattern.  We don't have data read/write capabilities yet.  In the meantime, let's reset the pattern, so we can at least guarantee there won't be garbage in the pattern. */
+	numberOfRows = MAX_NUMBER_OF_ROWS;
+
+	for (row = 0; row < MAX_NUMBER_OF_ROWS; row++) {
+		pattern[row][PITCH] = 24;
+		pattern[row][SLIDE] = 0;
+		pattern[row][GATE] = 0;
+		pattern[row][ACCENT] = 0;
+	}
 }
 
 void savePattern()
 {
+}
+
+void dacWrite(short twelveBits)
+{
+	byte firstFourBits = twelveBits >> 4;
+	byte lastEightBits = twelveBits & 255;
+	Wire.beginTransmission(96);
+	Wire.write(64); /* Set the DAC to receive new data. */
+	Wire.write(firstFourBits);
+	Wire.write(lastEightBits);
+	Wire.endTransmission();
 }
 
 /*
@@ -71,7 +123,91 @@ void savePattern()
 
 void setup()
 {
-	pinMode(13, OUTPUT);
+	pinMode(2, OUTPUT); /* Gate */
+	pinMode(3, OUTPUT); /* Accent */
+	pinMode(13, OUTPUT); /* The beat flashes the internal LED */
+	loadPattern();
+
+	/* Load a preset pattern into the first slot */
+	pattern[0][PITCH] = 28;
+	pattern[0][SLIDE] = 0;
+	pattern[0][GATE] = 0;
+	pattern[0][ACCENT] = 0;
+
+	pattern[1][PITCH] = 28;
+	pattern[1][SLIDE] = 0;
+	pattern[1][GATE] = 0;
+	pattern[1][ACCENT] = 0;
+
+	pattern[2][PITCH] = 28;
+	pattern[2][SLIDE] = 0;
+	pattern[2][GATE] = 35;
+	pattern[2][ACCENT] = 0;
+
+	pattern[3][PITCH] = 28;
+	pattern[3][SLIDE] = 0;
+	pattern[3][GATE] = 0;
+	pattern[3][ACCENT] = 0;
+
+	pattern[4][PITCH] = 35;
+	pattern[4][SLIDE] = 60;
+	pattern[4][GATE] = 60;
+	pattern[4][ACCENT] = 0;
+
+	pattern[5][PITCH] = 40;
+	pattern[5][SLIDE] = 60;
+	pattern[5][GATE] = 0;
+	pattern[5][ACCENT] = 0;
+
+	pattern[6][PITCH] = 28;
+	pattern[6][SLIDE] = 0;
+	pattern[6][GATE] = 0;
+	pattern[6][ACCENT] = 0;
+
+	pattern[7][PITCH] = 23;
+	pattern[7][SLIDE] = 0;
+	pattern[7][GATE] = 35;
+	pattern[7][ACCENT] = 0;
+
+	pattern[8][PITCH] = 31;
+	pattern[8][SLIDE] = 60;
+	pattern[8][GATE] = 60;
+	pattern[8][ACCENT] = 0;
+
+	pattern[9][PITCH] = 16;
+	pattern[9][SLIDE] = 60;
+	pattern[9][GATE] = 0;
+	pattern[9][ACCENT] = 0;
+
+	pattern[10][PITCH] = 28;
+	pattern[10][SLIDE] = 0;
+	pattern[10][GATE] = 0;
+	pattern[10][ACCENT] = 0;
+
+	pattern[11][PITCH] = 28;
+	pattern[11][SLIDE] = 0;
+	pattern[11][GATE] = 35;
+	pattern[11][ACCENT] = 0;
+
+	pattern[12][PITCH] = 28;
+	pattern[12][SLIDE] = 0;
+	pattern[12][GATE] = 0;
+	pattern[12][ACCENT] = 0;
+
+	pattern[13][PITCH] = 28;
+	pattern[13][SLIDE] = 0;
+	pattern[13][GATE] = 0;
+	pattern[13][ACCENT] = 0;
+
+	pattern[14][PITCH] = 28;
+	pattern[14][SLIDE] = 0;
+	pattern[14][GATE] = 0;
+	pattern[14][ACCENT] = 0;
+
+	pattern[15][PITCH] = 28;
+	pattern[15][SLIDE] = 0;
+	pattern[15][GATE] = 0;
+	pattern[15][ACCENT] = 0;
 }
 
 /*
@@ -114,24 +250,49 @@ void loop()
  */
 
 	if (clockPulseStarting == HIGH) {
-		oddPulse = oddPulse ^ 1; /* I'm guessing there's no such thing as "oddPulse =^ 1"? */
-
-		if (oddPulse == 0) {
+		if ((oddPulse = oddPulse ^ 1) == 0) {
+			/* It's an even numbered pulse.  So we're stepping down from 96 PPQN (which the clock uses) to 48 PPQN (which the sequencer uses). */
 			sequencerPulseCount++;
 
+			/* Once we reach the end of the pattern, loop back to the beginning, and load in the queued pattern if necessary. */
 			if (sequencerPulseCount > SEQUENCER_PPSN * numberOfRows) {
 				sequencerPulseCount = 0;
-				currentPatternNumber = nextPatternNumber;
-				loadPattern();
+
+				if (patternNumber != patternNumberRequest) {
+					patternNumber = patternNumberRequest;
+					loadPattern();
+				}
 			}
 
+			/* Work out which row we're on.  If it's changed, note the last row we were on, for slides. */
 			newCurrentRowNumber = sequencerPulseCount / SEQUENCER_PPSN;
 
-			if (newCurrentRowNumber != currentRowNumber) {
-				lastRowNumber = currentRowNumber;
+			if (newCurrentRowNumber != rowNumber) {
+				lastRowNumber = rowNumber;
 			}
 
-			currentRowNumber = newCurrentRowNumber;
+			rowNumber = newCurrentRowNumber;
+
+			/* Work out the current event's pitch, slide, gate length and accent */
+			pitchRequest = pattern[rowNumber][PITCH] * FROM_SIXTIETHS_TO_TWELVE_BITS;
+
+			if (pattern[lastRowNumber][SLIDE] == 0) {
+				slide = LOW;
+			} else {
+				slide = HIGH;
+			}
+
+			if (sequencerPulseCount % SEQUENCER_PPSN < pattern[rowNumber][GATE] * FROM_SIXTIETHS_TO_PULSES) {
+				gate = HIGH;
+			} else {
+				gate = LOW;
+			}
+
+			if (pattern[rowNumber][ACCENT] > 0) {
+				accent = HIGH;
+			} else {
+				accent = LOW;
+			}
 		}
 	}
 
@@ -139,19 +300,18 @@ void loop()
  *  Run the pitch through the slew limiter
  */
 
-/* Make sure pitchRequest and slide are set appropriately by the sequencer first */
 	slewLimiterInterval = clockTime - slewLimiterLastTime;
 	slewLimiterLastTime = clockTime;
 
-	if (slide == 0) {
-		slewLimiterPitch = pitchRequest;
+	if (slide == LOW) {
+		pitch = pitchRequest;
 	} else {
-		if (pitchRequest > slewLimiterPitch + (slewLimiterInterval * 2)) {
-			slewLimiterPitch = slewLimiterPitch + slewLimiterInterval;
-		} else if (pitchRequest < slewLimiterPitch - (slewLimiterInterval * 2)) {
-			slewLimiterPitch = slewLimiterPitch - slewLimiterInterval;
+		if (pitchRequest > pitch + (slewLimiterInterval * 2)) {
+			pitch = pitch + slewLimiterInterval;
+		} else if (pitchRequest < pitch - (slewLimiterInterval * 2)) {
+			pitch = pitch - slewLimiterInterval;
 		} else {
-			slewLimiterPitch = pitchRequest;
+			pitch = pitchRequest;
 		}
 	}
 
@@ -159,8 +319,12 @@ void loop()
  *  Set output
  */
 
+	/*dacWrite(pitch);*/
+	digitalWrite(2, gate);
+	digitalWrite(3, accent);
+
 	/* Flash the Arduino's internal LED on every beat */
-	if (currentRowNumber % 4 == 0) {
+	if (rowNumber % 4 == 0) {
 		digitalWrite(13, HIGH);
 	} else {
 		digitalWrite(13, LOW);
@@ -170,17 +334,72 @@ void loop()
  *  Get input
  */
 
+	if (0) { /* Toggle run/stop */
+		if (clockRunRequest == HIGH) {
+			clockRunRequest = LOW;
+			sequencerPulseCount = (SEQUENCER_PPSN * numberOfRows) - 1;
+		} else {
+			clockRunRequest = HIGH;
+			patternNumberRequest = patternNumber;
+		}
+	}
+
 	if (0) { /* Decrement the clock's tempo */
 		if (clockTempo > 1) {
 			clockTempo--;
-			clockPulseLength = CLOCK_FROM_TEMPO_TO_MILLISECONDS / clockTempo;
+			clockPulseLength = FROM_TEMPO_TO_MILLISECONDS / clockTempo;
 		}
 	}
 
 	if (0) { /* Increment the clock's tempo */
 		if (clockTempo < 255) {
 			clockTempo++;
-			clockPulseLength = CLOCK_FROM_TEMPO_TO_MILLISECONDS / clockTempo;
+			clockPulseLength = FROM_TEMPO_TO_MILLISECONDS / clockTempo;
 		}
+	}
+
+	if (0) { /* Add a row to the pattern */
+		if (numberOfRows < MAX_NUMBER_OF_ROWS) {
+			numberOfRows++;
+		}
+	}
+
+	if (0) { /* Remove a row from the pattern */
+		if (numberOfRows > 1) {
+			numberOfRows--;
+
+			/* Ensure the cursor doesn't stray from the still existent rows */
+			if (rowNumber > numberOfRows - 1) {
+				rowNumber = numberOfRows - 1;
+			}
+
+			/* Clear the removed row */
+			pattern[numberOfRows][PITCH] = 24;
+			pattern[numberOfRows][SLIDE] = 0;
+			pattern[numberOfRows][GATE] = 0;
+			pattern[numberOfRows][ACCENT] = 0;
+		}
+	}
+
+	if (0) { /* Copy a pattern to the clipboard */
+		for (row = 0; row < MAX_NUMBER_OF_ROWS; row++) {
+			for (item = 0; item < 4; item++) {
+				clipboard[row][item] = pattern[row][item];
+			}
+		}
+
+		clipboardNumberOfRows = numberOfRows;
+		clipboardFull = HIGH;
+	}
+
+	if (0) { /* Paste a pattern from the clipboard */
+		for (row = 0; row < MAX_NUMBER_OF_ROWS; row++) {
+			for (item = 0; item < 4; item++) {
+				pattern[row][item] = clipboard[row][item];
+			}
+		}
+
+		numberOfRows = clipboardNumberOfRows;
+		clipboardFull = LOW;
 	}
 }
